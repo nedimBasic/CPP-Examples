6c2ae68b06d1ff2a0901565597c82cf3d5f1b94d
nedim.basic <nedim.basic@ATOM068.atom.solutions>
2017-11-21 10:02:30 +0300

    Desktop application regarding calsulation parameters in circle. Circle Calculator v 2.0

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -6,10 +6,24 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+
 #include <iostream>
+#include <stdio.h>
+
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+
+int main()
+{
+    int i;
+    string broj;
+    char buffer[100];
+    getline(cin, broj);
+    cout<<"---------"<<endl;
+    //cin>>i;
+    i=stringToInt(buffer);
+    cout<<"broj je" << i;
+//   tochar(i, buffer);
+//   cout<<"karakter je "<<buffer;
+   return 0;
 }
